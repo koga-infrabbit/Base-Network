bb45fbed4deec172dfa9ad0cb75ec5c22849a311
Keisuke Koge <koga@infrabbit.com>
2023-07-06 17:34:48 +0900

    20230705-commit

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
+# AWS API Credentials
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 # VPC Network BaseSegment(Local)
 variable "vpc_address" {
     description = "VPC Network BaseSegment(Local)"
     type = string
     default = "10.0.0.0"
 }
+
